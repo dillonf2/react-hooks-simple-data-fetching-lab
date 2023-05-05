aaede5331a6adfbfa0f25b17f03bed592b3a78ae
Frank Dillon <dillonf2@outlook.com>
2023-05-05 17:31:23 -0400

    simple data fetching lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React, { useState,useEffect } from "react"
+
+
+function App(){
+    const [dogState,setDogState]=useState("")
+    useEffect(()=>{
+        fetch (`https://dog.ceo/api/breeds/image/random`)
+        .then(res=>res.json())
+        .then(data=>{
+            setDogState(data.message)
+        })
+    },[])
+
+    const checkState=()=>{
+        if (dogState===""){
+            return <p>Loading...</p>
+        }
+        if (dogState!==""){
+            return <img alt="A Random Dog" src={dogState} href={dogState}/>
+        }
+    }
+    console.log(dogState.message)
+    return (
+        <div>
+            {checkState()}
+        </div>
+    )
+}
+
+export default App
